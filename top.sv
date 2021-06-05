27e01ba153b6e3e19465fa1e28e439d486a24951
David Banks <dave@hoglet.com>
2021-06-05 15:41:17 +0100

    Revert to original top.sv

--- top.sv
+++ top.sv
@@ -1,79 +1,75 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
+// Company: 
+// Engineer: 
+// 
 // Create Date: 06/03/2021 07:56:28 PM
-// Design Name:
+// Design Name: 
 // Module Name: top
-// Project Name:
-// Target Devices:
-// Tool Versions:
-// Description:
-//
-// Dependencies:
-//
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-//
+// 
 //////////////////////////////////////////////////////////////////////////////////
 
 module top(
     input clk,
     input reset,
-    inout wire [7:0] data_io,
+    inout logic [7:0] data_io,
     output reg [15:0] address,
-    output reg rwb,
+    output rwb,
     output phi2,
     output led_a,
     output wire resb
     );
 
     wire rdy = 1'b1;
-    wire WE, RE, rom_e, ram_e;
-    wire [7:0] read;
-    wire [7:0] write;
-    wire [7:0] read_bus;
-    wire [7:0] rom_read;
-    wire [7:0] ram_read;
-    wire [7:0] ram_write;
-    wire [15:0] AD;
-    wire [15:0] address_bus;
-
-
-    assign phi2 = clk;
-    assign resb = ~reset;
-    assign address_bus = AD;
-    //assign address = address_bus;
+    wire WE, RE, rom_e, ram_e; 
+    logic [7:0] read;
+    logic [7:0] write; 
+    logic [7:0] write_bus; 
+    logic [7:0] read_bus; 
+    reg [7:0] rom_read; 
+    reg [7:0] ram_read; 
+    reg [7:0] ram_write; 
+    logic [15:0] AD; 
+    logic [15:0] address_bus; 
+    logic [7:0] DO; 
+    logic [7:0] DI; 
+    
+    assign phi2 = clk; 
+    assign DO = write_bus; 
+    assign data_io = (WE || (RE && rom_e) || (RE && ram_e)) ? DO : 'bZ;
+    assign DI = data_io;
+    
+    assign resb = ~reset; 
+    assign address_bus = AD; 
+    //assign address = address_bus; 
     assign RE = ~WE;
-    assign led_a = resb;
-
-    assign rom_e = (address_bus >= 16'hc000) ? 1'b1 : 1'b0;
-    assign ram_e = (address_bus < 16'h8000) ? 1'b1 : 1'b0;
-    assign ram_write = (WE && ram_e) ? write : 'bZ;
-
-// Delay mux select signals to line up with RAM/ROM outputs
-    reg ram_select;
-    reg rom_select;
-    always @(posedge clk) begin
-        ram_select <= RE && ram_e;
-        rom_select <= RE && rom_e;
-    end
-    assign read = ram_select ? ram_read : (rom_select ? rom_read : read_bus);
-
-// Properly pipeline external data bus
-    reg [7:0] write_bus;
-    always @(posedge clk) begin
-        if (rdy) begin
-           address   <= address_bus;
-           rwb       <= RE;
-           write_bus <= write;
-        end
-    end
-    assign data_io = (!rwb) ? write_bus : ram_select ? ram_read : rom_select ? rom_read : 8'hZZ;
-    assign read_bus = data_io;
-
+    assign rwb = RE; 
+    assign led_a = resb; 
+    
+    assign rom_e = (address_bus >= 16'hc000) ? 1'b1 : 1'b0; 
+    assign ram_e = (address_bus < 16'h8000) ? 1'b1 : 1'b0; 
+    
+    assign ram_write = (WE && ram_e) ? write : 'bZ; 
+    assign read_bus = DI; 
+    
+    assign read = (RE && ram_e) ? ram_read : ((RE && rom_e) ? rom_read : read_bus); 
+    assign write_bus = (WE) ? write : ((RE && rom_e) ? rom_read : ram_read); 
+    
+    //external address pads
+    always @(posedge clk)
+    if( rdy )
+        address <= address_bus;
+    
 //debouncer debouncer_a(
     //.clk(clk),
     //.switch_input(reset),
@@ -82,15 +78,15 @@ module top(
     //.trans_dn()
     //);
 
-cpu_65c02 cpu_alpha(
+cpu_65c02 cpu_alpha( 
      .clk(clk),                          // CPU clock
      .reset(reset),                          // RST signal
-     .AB(AD),                   // address bus
+     .AB(AD),                   // address bus 
      .DI(read),                     // data bus input
-     .DO(write),                // data bus output
+     .DO(write),                // data bus output 
      .WE(WE),                          // write enable
-     .IRQ(1'b0),                          // interrupt request
-     .NMI(1'b0),                          // non-maskable interrupt request
+     .IRQ(),                          // interrupt request
+     .NMI(),                          // non-maskable interrupt request
      .RDY(rdy)                         // Ready signal. Pauses CPU when RDY=0
      );                      // debug for simulation
 
